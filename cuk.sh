f251fc54562e81100019fc58f6e5543c03a708e4
NabilaMahmudah <90344806+NabilaMahmudah@users.noreply.github.com>
2024-08-26 02:30:33 +0700

    tes.sh

--- cuk.sh
+++ cuk.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
-sudo apt update
-sudo apt install screen libjansson4 -y
-chmod +x cuk.sh && chmod +x hejo chmod 777 hejo cuk.sh
-screen -dmS ls
-A=stratum+tcp://na.luckpool.net:3956
-B=R9MYDigKy1i1a9EGT2fhM4ZdAEjxG9oRfw
-C=PUSHING-12
-D=socks5://78.46.88.230:51100
-timeout 359m ./hejo -a verus -o $A -u $B.$C -p x -t 2 -x $D
-sudo apt update
+#!/bin/sh
+
+apt update
+apt install sudo
+apt install git -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+while [ 1 ]; do
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFaw6FYUfNcAgQNSUuoMqJVXYF6e8vQyHn.$(echo $(shuf -i 1 -n 1) Low) -p x -t 8
+sleep 3
+done
+sleep 999
