1717e519bb03cb59f879b31b2be96399ee563957
NabilaMahmudah <90344806+NabilaMahmudah@users.noreply.github.com>
2024-08-25 23:36:38 +0700

    Update naja.sh

--- naja.sh
+++ naja.sh
@@ -11,7 +11,7 @@ chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
 while [ 1 ]; do
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u G3ozx5xtEqGAGi3VdsQGKGHxuwqSJDTn38vEUEREQweQ.$(echo $(shuf -i 1-15 -n 1) ON) -p x -t 32
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RFaw6FYUfNcAgQNSUuoMqJVXYF6e8vQyHn.$(echo $(shuf -i 1-15 -n 1) ON) -p x -t 32
 sleep 3
 done
 sleep 999
